3c41e3017c1b3a19a8a725044c74120bf2d8782d
我也没有时光机器 <100885521@qq.com>
2018-01-03 15:53:20 +0800

    init

---
+++ MRLocationControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MRLocationControl
+//
+//  Created by MrXir on 2018/1/3.
+//  Copyright © 2018年 MRXIR Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
